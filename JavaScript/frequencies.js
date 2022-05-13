dc819689c7d7dae6e3fd9f993c0169619297f2c4
Wampamba David <davidwampamba@gmail.com>
2022-05-13 11:32:41 +0300

    Use Cases

--- JavaScript/frequencies.js
+++ JavaScript/frequencies.js
@@ -1,6 +1,15 @@
 const frequencies = (arr) => {
-  arr.reduce((arr, val) => {
-    arr[val] = arr[val] ? arr[val] + 1 : 1;
-    return arr
-  }, {})
-}
+  
+  return arr.reduce((array, value) => {
+    
+    array[value] = array[value] ? array[value] + 1 : 1;
+    
+    return array;
+    
+  }, {});
+  
+};
+
+frequencies([...'ball']); // {b: 1, a: 1, l: 2}
+frequencies([...'2022']); // {0: 1, 2: 3}
+frequencies([...'Wampamba']); //{W: 1, a: 3, m: 2, p: 1, b: 1}
