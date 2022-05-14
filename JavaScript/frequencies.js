78c3dacb0ef735deb1cf91158a7747b3a86e47b5
Wampamba David <davidwampamba@gmail.com>
2022-05-14 10:40:17 +0300

    Frequencies version 2

--- JavaScript/frequencies.js
+++ JavaScript/frequencies.js
@@ -15,3 +15,10 @@ const frequencies = (arr) => {
 frequencies([...'ball']); // {b: 1, a: 1, l: 2}
 frequencies([...'2022']); // {0: 1, 2: 3}
 frequencies([...'Wampamba']); //{W: 1, a: 3, m: 2, p: 1, b: 1}
+/*
+Version 2
+*/
+const frequencies2 = arr => arr.reduce((a, v) => { 
+  a[v] = (a[v] || 0 ) + 1;
+  return a;
+},{});
